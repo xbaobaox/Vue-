5f40157931d4841d35adb74837e915929c71fd46
xbaobaox <xbaobaox@sina.com>
2019-10-16 13:45:54 +0800

    10.16

--- webpack的使用/webpack的loader copy/src/js/info.js
+++
@@ -1,4 +0,0 @@
-let name = 'tom'
-let age = '18'
-let home = 'china'
-export {name,age,home}
--- webpack的使用/webpack的loader copy/src/main.js
+++
@@ -1,9 +0,0 @@
-import { add, mul } from './js/mathUtils';
-console.log(add(20, 30));
-console.log(mul(20, 30));
-import { name, age, home } from "./js/info.js"
-console.log(name, age, home)
-//依赖css文件
-require('./css/normal.css')
-//依赖less文件
-require('./css/special.less')
