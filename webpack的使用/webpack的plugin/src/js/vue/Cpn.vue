ff88806f724a76a0037ead7e2a0705f9d80ff3ea
xbaobaox <xbaobaox@sina.com>
2019-10-16 21:59:56 +0800

    10.16

---
+++ webpack的使用/webpack的plugin/src/js/vue/Cpn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Cpn组件标题</h2>
+    <p>Cpn内容</p>
+    <h3>{{name}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cpn",
+  data() {
+    return {
+      name: "Tom cpn",
+      message: "Cpn message"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
